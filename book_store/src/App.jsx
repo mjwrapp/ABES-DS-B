c0c46baa84954fe862d1f2358ef8604ea8e8f870
mjwrapp <joshi789mahi2@gmail.com>
2026-01-29 11:37:15 +0530

    starting new

---
+++ book_store/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import book from './component/book.jsx'
+
+function App() {
+  return (
+    <div>
+     <book />
+    </div>
+  )
+}
+export default book
+export default App
---
+++ book_store/src/component/book.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function book( props) {
+  return (
+    <div id="book">
+      <img src ="alt" height={100}width={100}> </img>
+      <h1>Title: {props.title}</h1>
+      <h1>Price: {props.price}</h1>
+    </div>
+   <div>
+    
+
+  )
+}
+
+export default book
